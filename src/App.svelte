709cadc762dd06d736c1ae52505fc17e034f0f10
Jitendra Chaudhary <jitendrac.itpath@gmail.com>
2023-03-29 18:45:01 +0530

    #293 - Added product,Home,footer & Navbar UI

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,25 @@
 <script lang="ts">
     import Navbar from "./Navbar.svelte";
 	import Footer from "./Footer.svelte";
+    import Home from "./Home.svelte";
+    import Product from "./Product.svelte";
 </script>
+  
 
 <Navbar />
 
+<Home />
+
+<br>
+<br>
+<br>
+<br>
+<br>
+
+<Product />
+
 <Footer />
+
 <style>
 
 </style>
--- src/Navbar.svelte
+++ src/Navbar.svelte
@@ -1,42 +1,44 @@
 <script lang="ts">
-    let showMenu:boolean = false;
-  
-    function toggleNavbar():void {
-      showMenu = !showMenu;
-    }
-  </script>
-  
-  <div>
-      <div class="bg-gray-800">
-        <nav class="container px-6 py-4">
-          <div>
-              <div class="flex items-center justify-between mb-4">
-                  <img src="../images/logo.webp" alt="LOGO" class="w-12 h-12 md:w-30">
-              <!-- svelte-ignore a11y-click-events-have-key-events -->
-              <div on:click={toggleNavbar} class="flex md:hidden">
-                  <i class="fa-solid fa-bars text-white text-3xl"></i>
+  let showMenu:boolean = false;
+
+  function toggleNavbar():void {
+    showMenu = !showMenu;
+  }
+</script>
+
+<div class="nav-bar bg-gray-700 fixed left-0 right-0">
+    <nav class="container px-6 py-4">
+      <div>
+          <div class="flex text-white">
+              <img src="../images/logo.webp" alt="LOGO" class="w-12 h-12 md:w-30">
+              
+              <div class="ml-3">
+                  <div>[Title]</div>
+                  <div>[Tag line]</div>
               </div>
-              </div>
-  
-              <div class="flex-col mt-8 space-y-4
-                  md:flex 
-                  md:space-y-0 
-                  md:flex-row 
-                  md:items-center 
-                  md:space-x-16 -space-x-16 
-                  md:mt-0 
-              {showMenu? 'flex': 'hidden'}">
-                  <span class="text-white hover:text-white-800">Home</span>
-                  <span class="text-white hover:text-white-800">Products</span>
-                  <span class="text-white hover:text-white-800">Contact Us</span>
-                  <span class="text-white hover:text-white-800">About Us</span>
-                  <span class="text-white hover:text-white-800">Admin</span>
-              </div>
+
+            <!-- svelte-ignore a11y-click-events-have-key-events -->
+            <div on:click={toggleNavbar} class="flex md:hidden absolute right-8 mt-2">
+                <i class="fa-solid fa-bars text-white text-3xl"></i>
+            </div>
           </div>
-        </nav>
+          <div class="flex-col space-y-4 
+              md:flex 
+              md:ml-8
+              md:space-y-0 
+              md:flex-row 
+              md:items-center
+              md:justify-between
+              md:mt-0  {showMenu? 'flex': 'hidden'}">
+            <div class="text-white hover:text-white-800 flex-col text-white md:pt-4 pt-4">Home</div>
+            <div class="text-white hover:text-white-800 flex-col text-white md:pt-4">Products</div>
+            <div class="text-white hover:text-white-800 flex-col text-white md:pt-4">Contact Us</div>
+            <div class="text-white hover:text-white-800 flex-col text-white md:pt-4">About Us</div>
+            <div class="text-white hover:text-white-800 flex-col text-white md:pt-4">Admin</div>
+          </div>
       </div>
-    </div>
-  
-  <style>
-  
-  </style>
+    </nav>
+</div>
+<div class="my-24 md:my-36"></div>
+
+
