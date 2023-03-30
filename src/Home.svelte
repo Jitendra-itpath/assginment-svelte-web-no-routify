14c71b47f3f503fe732931a9ce8c61c1c44295d8
Jitendra Chaudhary <jitendrac.itpath@gmail.com>
2023-03-30 16:50:13 +0530

    #303 - added add product Ui and made some more Ui changes.

---
+++ src/Home.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+    
+</script>
+  
+
+<div class="home mt-5 md:mx-8 mx-4">
+    <div class="grid md:grid-cols-2 lg:gap-24 md:gap-16 gap-10">
+        <div class="flex justify-center">
+            <div
+              class="block max-w-fit rounded-lg bg-white p-6 shadow-lg">
+              <h5
+                class="mb-2 text-xl font-medium leading-tight text-neutral-800 ">
+                Products
+              </h5>
+              <p class="mb-4 text-base text-neutral-600 text-justify">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+              <button type="button" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4  focus:outline-none focus:ring-blue-300  font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
+                View All
+              </button>
+            </div>
+          </div>
+
+          <div class="flex justify-center">
+            <div
+              class="block max-w-fit rounded-lg bg-white p-6 shadow-lg ">
+              <h5
+                class="mb-2 text-xl font-medium leading-tight text-neutral-800">
+                Contact US
+              </h5>
+              <p class="mb-4 text-base text-neutral-600 text-justify">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+              <button type="button" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4  focus:outline-none focus:ring-blue-300  font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
+                Contact US Now
+              </button>
+            </div>
+          </div>
+    </div>
+
+    <div class="mt-10">
+        <p class="text-5xl font-bold mb-4 text-neutral-600 leading-loose">Heading</p>
+        <p class="mb-4 text-xl text-neutral-600 text-justify">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+    </div>
+</div>
+
+<style>
+
+</style>
