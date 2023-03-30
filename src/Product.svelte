843bba9db3b3bfa2705937b4d39eacfb0fcd019a
Jitendra Chaudhary <jitendrac.itpath@gmail.com>
2023-03-30 10:21:44 +0530

    #293 - changes made and Added product,Home,footer & Navbar UI

---
+++ src/Product.svelte
@@ -0,0 +1,118 @@
+<script lang="ts">
+    
+</script>
+  
+
+<div class="relative mb-16"> 
+    <div class="absolute right-0">
+        <button type="button" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 
+        hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 
+        font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
+        Add new product
+        </button>
+    </div>
+</div>
+<div class="block md:mx-10 mx-2">
+    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <table class="w-full text-sm text-left text-gray-500 ">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50">
+                <tr>
+                    <th scope="col" class="px-6 py-3">
+                        Name
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Price
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Description
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Action
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="bg-white border-b dark:bg-gray-900 ">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap ">
+                        Apple MacBook Pro 17"
+                    </th>
+                    <td class="px-6 py-4">
+                        $2999
+                    </td>
+                    <td class="px-6 py-4">
+                        Laptop
+                    </td>
+                    <td class="px-6 py-4">
+                        <a href="/#" class="font-medium text-blue-600   hover:underline"><i class="text-2xl fa-solid fa-ellipsis"></i></a>
+                    </td>
+                </tr>
+                <tr class="border-b bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap ">
+                        Microsoft Surface Pro
+                    </th>
+                    <td class="px-6 py-4">
+                        $1999
+                    </td>
+                    <td class="px-6 py-4">
+                        Laptop PC
+                    </td>
+                    <td class="px-6 py-4">
+                        <a href="/#" class="font-medium text-blue-600   hover:underline"><i class="text-2xl fa-solid fa-ellipsis"></i></a>
+                    </td>
+                </tr>
+                <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap ">
+                        Magic Mouse 2
+                    </th>
+                    <td class="px-6 py-4">
+                        $99
+                    </td>
+                    <td class="px-6 py-4">
+                        Accessories
+                    </td>
+                    <td class="px-6 py-4">
+                        <a href="/#" class="font-medium text-blue-600   hover:underline"><i class="text-2xl fa-solid fa-ellipsis"></i></a>
+                    </td>
+                </tr>
+                <tr class="border-b bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap ">
+                        Google Pixel Phone
+                    </th>
+                    <td class="px-6 py-4">
+                        $799
+                    </td>
+                    <td class="px-6 py-4">
+                        Phone
+                    </td>
+                    <td class="px-6 py-4">
+                        <a href="/#" class="font-medium text-blue-600   hover:underline"><i class="text-2xl fa-solid fa-ellipsis"></i></a>
+                    </td>
+                </tr>
+                <tr>
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap ">
+                        Apple Watch 5
+                    </th>
+                    <td class="px-6 py-4">
+                        $999
+                    </td>
+                    <td class="px-6 py-4">
+                        Wearables
+                    </td>
+                    <td class="px-6 py-4">
+                        <a href="/#" class="font-medium text-blue-600   hover:underline"><i class="text-2xl fa-solid fa-ellipsis"></i></a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+
+<br>
+<br>
+<br>
+<br>
+<br>
+
+<style>
+
+</style>
